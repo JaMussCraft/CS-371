07da06e72f55edeaf60d343cb2b594c0d325ff42
Sing Hieng (James) Wong <99109398+JaMussCraft@users.noreply.github.com>
2025-03-01 00:31:13 +0000

    Initial working version complete

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # CS-371
 This repo is for CS 371 programming assignments done by James, Ayah, and Hannah. 
+
+### Compilation
+```bash
+gcc -o pa1_skeleton pa1_skeleton.c -pthread 
+```
+
+### Starting Server
+```bash
+./pa1_skeleton server 127.0.0.1 12345
+```
+
+### Running Clients
+```bash
+./pa1_skeleton client 127.0.0.1 12345 4 1000000
+```
--- pa1_skeleton.c
+++ pa1_skeleton.c
@@ -19,9 +19,9 @@
 /* 
 Please specify the group members here
 
-# Student #1: 
-# Student #2:
-# Student #3: 
+# Student #1: Sing Hieng (James) Wong
+# Student #2: Ayah Abdeldayem
+# Student #3: Hannah Lynn
 
 */
 
@@ -34,6 +34,7 @@ Please specify the group members here
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <pthread.h>
+#include <errno.h>
 
 #define MAX_EVENTS 64
 #define MESSAGE_SIZE 16
@@ -47,7 +48,8 @@ int num_requests = 1000000;
 /*
  * This structure is used to store per-thread data in the client
  */
-typedef struct {
+typedef struct
+{
     int epoll_fd;        /* File descriptor for the epoll instance, used for monitoring events on the socket. */
     int socket_fd;       /* File descriptor for the client socket connected to the server. */
     long long total_rtt; /* Accumulated Round-Trip Time (RTT) for all messages sent and received (in microseconds). */
@@ -58,25 +60,56 @@ typedef struct {
 /*
  * This function runs in a separate client thread to handle communication with the server
  */
-void *client_thread_func(void *arg) {
+void *client_thread_func(void *arg)
+{
     client_thread_data_t *data = (client_thread_data_t *)arg;
     struct epoll_event event, events[MAX_EVENTS];
-    char send_buf[MESSAGE_SIZE] = "ABCDEFGHIJKMLNOP"; /* Send 16-Bytes message every time */
-    char recv_buf[MESSAGE_SIZE];
-    struct timeval start, end;
+    char write_buf[MESSAGE_SIZE] = "ABCDEFGHIJKMLNOP"; /* Send 16-Bytes message every time */
+    char read_buf[MESSAGE_SIZE];
+    struct timeval start, end;  // Timestamps for RTT measurement
 
-    // Hint 1: register the "connected" client_thread's socket in the its epoll instance
-    // Hint 2: use gettimeofday() and "struct timeval start, end" to record timestamp, which can be used to calculated RTT.
 
     /* TODO:
      * It sends messages to the server, waits for a response using epoll,
      * and measures the round-trip time (RTT) of this request-response.
-     */
- 
-    /* TODO:
      * The function exits after sending and receiving a predefined number of messages (num_requests). 
      * It calculates the request rate based on total messages and RTT
      */
+
+    // Register socket with epoll for both read and write events
+    event.events = EPOLLIN | EPOLLOUT;
+    event.data.fd = data->socket_fd;
+    if (epoll_ctl(data->epoll_fd, EPOLL_CTL_ADD, data->socket_fd, &event) == -1)
+    {
+        perror("epoll_ctl: listen_sock");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < num_requests; i++)
+    {
+        gettimeofday(&start, NULL);
+        write(data->socket_fd, write_buf, MESSAGE_SIZE);
+
+        int nfds = epoll_wait(data->epoll_fd, events, MAX_EVENTS, -1);
+        if (nfds == -1)
+        {
+            perror("epoll_wait");
+            exit(EXIT_FAILURE);
+        }
+
+        read(data->socket_fd, read_buf, MESSAGE_SIZE);
+        gettimeofday(&end, NULL);
+
+        // record the total_rtt in microseconds
+        data->total_rtt += (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_usec - start.tv_usec);
+        data->total_messages++;
+    }
+
+    // request_rate in messages per second
+    data->request_rate = (float)data->total_messages / ((float)data->total_rtt / 1000000);
+
+    close(data->socket_fd);
+    close(data->epoll_fd);
 
     return NULL;
 }
@@ -85,7 +118,8 @@ void *client_thread_func(void *arg) {
  * This function orchestrates multiple client threads to send requests to a server,
  * collect performance data of each threads, and compute aggregated metrics of all threads.
  */
-void run_client() {
+void run_client()
+{
     pthread_t threads[num_client_threads];
     client_thread_data_t thread_data[num_client_threads];
     struct sockaddr_in server_addr;
@@ -94,51 +128,210 @@ void run_client() {
      * Create sockets and epoll instances for client threads
      * and connect these sockets of client threads to the server
      */
-    
-    // Hint: use thread_data to save the created socket and epoll instance for each thread
-    // You will pass the thread_data to pthread_create() as below
-    for (int i = 0; i < num_client_threads; i++) {
+
+    // Initialize server address structure
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
+    server_addr.sin_port = htons(server_port);
+
+    // Create and initialize client threads
+    for (int i = 0; i < num_client_threads; i++)
+    {
+        // Create epoll instance
+        thread_data[i].epoll_fd = epoll_create1(0);
+        if (thread_data[i].epoll_fd == -1)
+        {
+            perror("epoll_create1");
+            exit(EXIT_FAILURE);
+        }
+
+        // Create socket
+        thread_data[i].socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (thread_data[i].socket_fd == -1)
+        {
+            perror("socket");
+            exit(EXIT_FAILURE);
+        }
+
+        // Connect to server
+        if (connect(thread_data[i].socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+        {
+            perror("connect");
+            exit(EXIT_FAILURE);
+        }
+
+        thread_data[i].total_messages = 0;
+        thread_data[i].total_rtt = 0;
+
         pthread_create(&threads[i], NULL, client_thread_func, &thread_data[i]);
     }
 
     /* TODO:
      * Wait for client threads to complete and aggregate metrics of all client threads
      */
+    long long total_rtt = 0;
+    long total_messages = 0;
+    float total_request_rate = 0;
+
+    for (int i = 0; i < num_client_threads; i++)
+    {
+        pthread_join(threads[i], NULL);
+        total_rtt += thread_data[i].total_rtt;
+        total_messages += thread_data[i].total_messages;
+        total_request_rate += thread_data[i].request_rate;
+        printf("Thread %d complete\n", i);
+    }
 
     printf("Average RTT: %lld us\n", total_rtt / total_messages);
     printf("Total Request Rate: %f messages/s\n", total_request_rate);
 }
 
-void run_server() {
+void run_server()
+{
+    printf("Server started!\n");
 
     /* TODO:
      * Server creates listening socket and epoll instance.
      * Server registers the listening socket to epoll
      */
+    int listen_sock, conn_sock, nfds, epollfd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len;
+    struct epoll_event event, events[MAX_EVENTS];
+    char buf[MESSAGE_SIZE];
+
+    // Create socket
+    listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_sock == -1)
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize server address structure
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(server_port);
+
+    // Bind listening socket to server address
+    if (bind(listen_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set socket to listening mode
+    if (listen(listen_sock, SOMAXCONN) == -1)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    epollfd = epoll_create1(0);
+    if (epollfd == -1)
+    {
+        perror("epoll_create1");
+        exit(EXIT_FAILURE);
+    }
+
+    event.events = EPOLLIN;
+    event.data.fd = listen_sock;
+    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listen_sock, &event) == -1)
+    {
+        perror("epoll_ctl: listen_sock");
+        exit(EXIT_FAILURE);
+    }
 
     /* Server's run-to-completion event loop */
-    while (1) {
+    while (1)
+    {
         /* TODO:
          * Server uses epoll to handle connection establishment with clients
          * or receive the message from clients and echo the message back
          */
+        nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+        if (nfds == -1)
+        {
+            perror("epoll_wait");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int n = 0; n < nfds; ++n)
+        {
+            if (events[n].data.fd == listen_sock)
+            {
+                // New connection event
+                client_len = sizeof(client_addr);
+                conn_sock = accept(listen_sock, (struct sockaddr *)&client_addr, &client_len);
+                if (conn_sock == -1)
+                {
+                    perror("accept");
+                    exit(EXIT_FAILURE);
+                }
+
+                event.events = EPOLLIN | EPOLLET;
+                event.data.fd = conn_sock;
+                if (epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_sock, &event) == -1)
+                {
+                    perror("epoll_ctl: conn_sock");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else
+            {
+                // Received data on existing connection
+                conn_sock = events[n].data.fd;
+                int read_len = read(conn_sock, buf, MESSAGE_SIZE);
+                if (read_len == 0)
+                {
+                    close(conn_sock);
+                }
+                else if (read_len < 0)
+                {
+                    if (errno != EAGAIN)
+                    {
+                        perror("read");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                else
+                {
+                    // Echo client's message back
+                    write(conn_sock, buf, MESSAGE_SIZE);
+                }
+            }
+        }
     }
 }
 
-int main(int argc, char *argv[]) {
-    if (argc > 1 && strcmp(argv[1], "server") == 0) {
-        if (argc > 2) server_ip = argv[2];
-        if (argc > 3) server_port = atoi(argv[3]);
+int main(int argc, char *argv[])
+{
+    if (argc > 1 && strcmp(argv[1], "server") == 0)
+    {
+        if (argc > 2)
+            server_ip = argv[2];
+        if (argc > 3)
+            server_port = atoi(argv[3]);
 
         run_server();
-    } else if (argc > 1 && strcmp(argv[1], "client") == 0) {
-        if (argc > 2) server_ip = argv[2];
-        if (argc > 3) server_port = atoi(argv[3]);
-        if (argc > 4) num_client_threads = atoi(argv[4]);
-        if (argc > 5) num_requests = atoi(argv[5]);
+    }
+    else if (argc > 1 && strcmp(argv[1], "client") == 0)
+    {
+        if (argc > 2)
+            server_ip = argv[2];
+        if (argc > 3)
+            server_port = atoi(argv[3]);
+        if (argc > 4)
+            num_client_threads = atoi(argv[4]);
+        if (argc > 5)
+            num_requests = atoi(argv[5]);
 
         run_client();
-    } else {
+    }
+    else
+    {
         printf("Usage: %s <server|client> [server_ip server_port num_client_threads num_requests]\n", argv[0]);
     }
 
